84692c5e7380750a762db59077e0c2d065f827c0
Shabeer <shbrkp@gmail.com>
2026-02-21 15:32:52 +0530

    product-section changes commit to local

---
+++ src/main/java/com/app/ecom/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.app.ecom.repository;
+
+import com.app.ecom.dto.ProductResponse;
+import com.app.ecom.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByActiveTrue();
+
+    // where p.active= true and p.stockQuantity>0
+    //@Query("SELECT p FROM products p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%',:keyword,'%'))")
+    @Query("SELECT p FROM products p WHERE p.stockQuantity>0 AND p.active = true AND p.name ILIKE %:keyword%")
+    List<Product> searchProduct(@Param("keyword") String keyword);
+}
