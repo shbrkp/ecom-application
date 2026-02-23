60f663bbcb8b295076609fe887154146f1a5f3c8
Shabeer <shbrkp@gmail.com>
2026-02-23 12:37:40 +0530

    order-section changes commit to local

---
+++ src/main/java/com/app/ecom/model/Order.java
@@ -0,0 +1,39 @@
+package com.app.ecom.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity(name = "orders")
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    private BigDecimal totalAmount;
+
+    private Enum<OrderStatus> status = OrderStatus.PENDING;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+}
---
+++ src/main/java/com/app/ecom/model/OrderItem.java
@@ -0,0 +1,25 @@
+package com.app.ecom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    private Integer quantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false )
+    private Order order;
+}
